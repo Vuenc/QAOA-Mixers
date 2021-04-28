5cde04e8bc0ebfccbc4893aa11d2b7a187f48230
Vuenc <v.buergin@gmx.de>
2021-04-28 18:29:19 +0200

    Changed integer attributes to not be vectors in parametric gates (don't need to be optimized by Flux). Minor fixes.

--- src/max-k-colorable-subgraph.jl
+++ src/max-k-colorable-subgraph.jl
@@ -11,10 +11,10 @@ struct Graph
         n >= 1 || throw(DomainError("n must be a positive integer"))
 
         # Turn into set of sets
-        edge_set = Set(map(Set, edges))
+        edge_set = Set(Set.(edges))
 
         # Verify that all edges are valid
-        all(edge -> edge ⊆ 1:n, edge_set) || throw(ArgumentError("Some edges have invalid endpoints"))
+        all(edge -> edge ⊆ 1:n && length(edge) == 2, edge_set) || throw(ArgumentError("Some edges have invalid endpoints"))
         new(n, edge_set)
     end
 end
@@ -37,24 +37,30 @@ struct MaxKColSubgraphPhaseSeparationGate <: AbstractGate
     # TODO possibly rename struct when this is in some module?
     # use a reference type (array with 1 entry) for compatibility with Flux
     γ::Vector{Float64} 
-    κ::Vector{Int} # the number of possible colors
+    κ::Int # the number of possible colors
     graph::Graph # the underlying graph which should be colored
 
     function MaxKColSubgraphPhaseSeparationGate(γ::Float64, κ::Integer, graph::Graph)
         κ > 0 || throw(ArgumentError("Parameter κ must be a positive integer!"))
-        new([γ], [κ], graph)
+        new([γ], κ, graph)
     end
 end
 
-function Qaintessent.matrix(g::MaxKColSubgraphPhaseSeparationGate)
+function phase_separation_hamiltonian(graph::Graph, κ::Int)
     Z = [1 0; 0 -1]
-
+    
     # Implementation of Eq. (17)
     # one-hot encoding: n * κ vector. Index (a-1)*n + (b-1) corresponds to vertex a, color b.
     H_P_enc = sum(
-        kron((color == a && vertex ∈ edge ? Z : I(2) for vertex ∈ 1:g.graph.n for color ∈ 1:g.κ[])...) # Z_{u,a} Z_{v,a}
-        for a ∈ 1:g.κ[] for edge in g.graph.edges # Σ_{(u,v) = edge ∈ E} Σ_{a=1..κ}
+        kron((color == a && vertex ∈ edge ? Z : I(2) for vertex ∈ 1:graph.n for color ∈ 1:κ)...) # Z_{u,a} Z_{v,a}
+        for a ∈ 1:κ for edge ∈ graph.edges # Σ_{(u,v) = edge ∈ E} Σ_{a=1..κ}
     )
+    H_P_enc
+end
+
+function Qaintessent.matrix(g::MaxKColSubgraphPhaseSeparationGate)
+    # Calculate the hamiltonian (Eq. 17)
+    H_P_enc = phase_separation_hamiltonian(g.graph, g.κ)
 
     # Implementation of one-hot phase seperator, A.3.1, p. 34
     U_P = exp(-im * g.γ[] * H_P_enc)
@@ -65,7 +71,7 @@ end
 Qaintessent.sparse_matrix(g::MaxKColSubgraphPhaseSeparationGate) = sparse(matrix(g))
 
 # wires
-Qaintessent.num_wires(g::MaxKColSubgraphPhaseSeparationGate)::Int = g.κ[] * g.graph.n
+Qaintessent.num_wires(g::MaxKColSubgraphPhaseSeparationGate)::Int = g.κ * g.graph.n
 
 function max_κ_colorable_subgraph_circuit(γs::Vector{Float64}, βs::Vector{Float64}, 
         graph::Graph, κ::Integer)
@@ -79,7 +85,7 @@ function max_κ_colorable_subgraph_circuit(γs::Vector{Float64}, βs::Vector{Flo
         push!(gates, CircuitGate(Tuple(1:N), MaxKColSubgraphPhaseSeparationGate(γ, κ, graph)))
 
         # Add the mixer, consisting of a partial mixer gate for each vertex
-        for vertex in 1:graph.n
+        for vertex ∈ 1:graph.n
             push!(gates, CircuitGate(Tuple(((vertex - 1) * κ + 1):(vertex * κ)), ParityRingMixerGate(β, κ)))
         end
     end
--- src/mixer-gates.jl
+++ src/mixer-gates.jl
@@ -16,25 +16,25 @@ Reference:\n
 struct RNearbyValuesMixerGate <: AbstractGate
     # use a reference type (array with 1 entry) for compatibility with Flux
     β::Vector{Float64}
-    r::Vector{Integer} # this is the r in r-Nearby-values
-    d::Vector{Integer} # d (= κ) = number of colors
+    r::Integer # this is the r in r-Nearby-values
+    d::Integer # d (= κ) = number of colors
 
     function RNearbyValuesMixerGate(β::Real, r::Integer, d::Integer)
         (r > 0 && d > 0) || throw(ArgumentError("Parameters r and d must be positive integers!"))
-        new([β], [r], [d])
+        new([β], r, d)
     end
 end
 
 # Implementation of Eq. (6)
 function matrix_onehot(g::RNearbyValuesMixerGate)
-    g.r[] == 1 || throw(DomainError("Matrix for one-hot encoding only implemented for r=1."))
+    g.r == 1 || throw(DomainError("Matrix for one-hot encoding only implemented for r=1."))
     
     X = [0 1; 1 0]
     Y = [0 -im; im 0]
     H_ring_enc = sum( # question: what does the paper mean by X_a for a = 0? Here, X_0 is just ignored
-        kron((i ∈ [a, a+1] ? X : I(2) for i ∈ 1:g.d[])...) # passing iterator into kron via varargs syntax
-        + kron((i ∈ [a, a+1] ? Y : I(2) for i ∈ 1:g.d[])...)
-        for a ∈ 0:g.d[]-1
+        kron((i ∈ [a, a+1] ? X : I(2) for i ∈ 1:g.d)...) # passing iterator into kron via varargs syntax
+        + kron((i ∈ [a, a+1] ? Y : I(2) for i ∈ 1:g.d)...)
+        for a ∈ 0:g.d-1
     )
     U_ring_enc = exp(-im * g.β[] * H_ring_enc)
 end
@@ -43,10 +43,10 @@ function matrix_qudit(g::RNearbyValuesMixerGate)
     # question: will this simulator work with qudits?
 
     # Implementation of Eq. (A3)
-    genX = I(g.d[])[:,vcat(2:g.d[], 1)] # move first columnn of identity matrix to back to obtain d-dim. generalized Pauli-X
+    genX = I(g.d)[:,vcat(2:g.d, 1)] # move first columnn of identity matrix to back to obtain d-dim. generalized Pauli-X
     genX_T = transpose(genX)
 
-    H_rNV = sum(genX^i + genX_T^i for i ∈ 1:g.r[]) # TODO implement more efficiently? (avoid exponentation for each i)
+    H_rNV = sum(genX^i + genX_T^i for i ∈ 1:g.r) # TODO implement more efficiently? (avoid exponentation for each i)
     U_rNV = exp(-im * g.β[] * H_rNV)
 
     U_rNV
@@ -60,7 +60,7 @@ Qaintessent.sparse_matrix(g::RNearbyValuesMixerGate) = sparse(matrix(g))
 
 # wires
 # TODO: should be d in the one-hot case, 1 for qudit case
-Qaintessent.num_wires(g::RNearbyValuesMixerGate)::Int = g.d[]
+Qaintessent.num_wires(g::RNearbyValuesMixerGate)::Int = g.d
 
 """
     Parity single-qudit ring mixer gate, which acts on a single qudit
@@ -93,19 +93,19 @@ function Qaintessent.matrix(g::ParityRingMixerGate)
     # Implements X_a X_{a+1} + Y_a Y_{a+1}, or more generally (⊗_{i ∈ xy_indices} X_i) + (⊗_{i ∈ xy_indices} Y_i)
     # question: what does the paper mean by X_a for a = d+1? Here, X_{d+1} is just ignored. Also, do they use 1-based indexing or 0-based? (cf. matrix_onehot above)
     XY_sum = xy_indices -> begin
-        kron((i ∈ xy_indices ? X : I(2) for i ∈ 1:g.d[])...) # passing iterator into kron via varargs syntax
-        + kron((i ∈ xy_indices ? Y : I(2) for i ∈ 1:g.d[])...)
+        kron((i ∈ xy_indices ? X : I(2) for i ∈ 1:g.d)...) # passing iterator into kron via varargs syntax
+        + kron((i ∈ xy_indices ? Y : I(2) for i ∈ 1:g.d)...)
     end
 
     # Implements Eq. (8)
     # question: by a ≠ n, do they actually mean a ≠ d? I assume so.
-    H_odd = sum(XY_sum([a, a+1]) for a ∈ 1:2:(g.d[] - 1))
-    H_even = sum(XY_sum([a, a+1]) for a ∈ 2:2:g.d[])
+    H_odd = sum(XY_sum([a, a+1]) for a ∈ 1:2:(g.d - 1))
+    H_even = sum(XY_sum([a, a+1]) for a ∈ 2:2:g.d)
     U_odd = exp(-im * g.β[] * H_odd)
     U_even = exp(-im * g.β[] * H_even)
 
     # Implements Eq. (9)
-    U_last = isodd(g.d[]) ? exp(-im * g.β[] * XY_sum([g.d[], 1])) : I
+    U_last = isodd(g.d) ? exp(-im * g.β[] * XY_sum([g.d, 1])) : I
 
     # Implements Eq. (7)
     U_parity = U_last * U_even * U_odd
@@ -115,4 +115,4 @@ end
 Qaintessent.sparse_matrix(g::ParityRingMixerGate) = sparse(matrix(g))
 
 # wires
-Qaintessent.num_wires(g::ParityRingMixerGate)::Int = g.d[]
+Qaintessent.num_wires(g::ParityRingMixerGate)::Int = g.d
